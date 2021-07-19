66c81f48eaaea3b46874b1fb1c56fe570c74dbbd
Julio <rafer2797@outlook.com>
2021-07-19 10:31:50 -0500

    finsht part 3

--- DjangoViewAuthorization/Blog/core/views.py
+++ DjangoViewAuthorization/Blog/core/views.py
@@ -2,6 +2,10 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from core.models import Blog
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib import messages
+
+
 
 
 def listing(request):
@@ -50,3 +54,17 @@ def user_info(request):
 @login_required
 def private_place(request):
     return HttpResponse("Shhh, members only!", content_type="text/plain")
+
+
+@user_passes_test(lambda user: user.is_staff)
+def staff_place(request):
+    return HttpResponse("Employees must wash hands", content_type="text/plain")
+
+#mensajes para los usuarios
+@login_required
+def add_messages(request):
+    username = request.user.username
+    messages.add_message(request, messages.INFO, f"Hello {username}")
+    messages.add_message(request, messages.WARNING, "DANGER WILL ROBINSON")
+
+    return HttpResponse("Messages added", content_type="text/plain")
